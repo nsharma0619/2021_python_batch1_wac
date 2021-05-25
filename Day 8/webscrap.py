ad5a6d988349f3f2c2876e2c9e2404c8ccaafd6f
Neeraj Sharma <nsharma0619@gmail.com>
2021-05-25 13:57:07 +0530

    day 9 completed

--- Day 8/webscrap.py
+++ Day 8/webscrap.py
@@ -1,12 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get("https://en.wikipedia.org/wiki/<query>")
+response = requests.get("https://www.indcareer.com/find/all-colleges-in-delhi?page=2")
 html_doc = response.text
 
 soup = BeautifulSoup(html_doc, 'html.parser')
 
-# print(soup.prettify())
+print(soup.prettify())
 
 # print(soup.title)
 # print(soup.title.text)
@@ -18,4 +18,4 @@ soup = BeautifulSoup(html_doc, 'html.parser')
 # for tag in soup.find_all('a'):
 #     print(tag.get('href'))
 
-print(soup.get_text())
+# print(soup.get_text())
